465d13dac080bbecae3195df33636d71ac132a1f
Matt Weichel <maweiche@gmail.com>
2021-11-29 19:04:33 -0800

    fixed apollo provider issue in app.js

---
+++ client/build/precache-manifest.aef8db6549c6653f40e7483dffdb6489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d13864ffe77db7f97196a987095a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f58a5da923d6e5a4174",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "70116fe2462c81417ff2",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "6f58a5da923d6e5a4174",
+    "url": "/static/js/2.11ee2f74.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.11ee2f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70116fe2462c81417ff2",
+    "url": "/static/js/main.f048b482.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
